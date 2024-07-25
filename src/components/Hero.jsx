536dd53334e1c7be8479201832339f254ce1576e
kayleeYuanyuan <yuanyuanma817@gmail.com>
2024-07-25 12:11:08 +1000

    second commit

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,6 +1,6 @@
 import { HERO_CONTENT } from "../constants";
-import profilePic from "../assets/kevinRushProfile.png";
-// import profilePic from "../assets/photo.jpg";
+// import profilePic from "../assets/kevinRushProfile.png";
+import profilePic from "../assets/photo.jpg";
 import { motion } from "framer-motion";
 
 const container = (delay) => ({
